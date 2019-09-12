80a06b5910eb5382694f9cfa952c6aeaf073c66a
David <54381677+DLis6@users.noreply.github.com>
2019-09-11 19:04:58 -0500

    Paress

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+var str = ""
+
+app.get('/', (req, res) => {
+
+    for (var i = 1; i <= 50; i++) {
+        if (i % 2 == 0) {
+            str += '<p>' + i + ' Soy par!</p>'
+        } else {
+            str += '<p>' + i + ' Soy impar!</p>'
+        }
+
+    }
+
+    res.send(str);
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
